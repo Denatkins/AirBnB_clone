b5473c19d5655e592c2f558d1960c22c009752ea
jentrixwameya <107076449+jentrixwameya@users.noreply.github.com>
2022-11-28 23:23:07 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -2,3 +2,12 @@
 """Module for State class."""
 
 from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
